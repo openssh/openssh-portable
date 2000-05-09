0e963ef3c5ce28a5b47395569858a90a4e7a1e87
Damien Miller <djm@mindrot.org>
2000-05-09 14:45:00 +1000

    oops - missed this

---
+++ openbsd-compat.h
@@ -0,0 +1,23 @@
+#ifndef _OPENBSD_H
+#define _OPENBSD_H
+
+#include "config.h"
+
+/* BSD function replacements */
+#include "bsd-bindresvport.h"
+#include "bsd-rresvport.h"
+#include "bsd-misc.h"
+#include "bsd-strlcpy.h"
+#include "bsd-strlcat.h"
+#include "bsd-mktemp.h"
+#include "bsd-snprintf.h"
+#include "bsd-daemon.h"
+#include "bsd-login.h"
+#include "bsd-base64.h"
+
+/* rfc2553 socket API replacements */
+#include "fake-getaddrinfo.h"
+#include "fake-getnameinfo.h"
+#include "fake-socket.h"
+
+#endif /* _OPENBSD_H */
