3b9734c700be8a31dc0966e58abe904b01da6492
Damien Miller <djm@mindrot.org>
2000-07-11 14:54:22 +1000

    - (djm) Added bsd-inet_aton and configure support for NeXT

--- bsd-inet_aton.h
+++ bsd-inet_aton.h
@@ -4,7 +4,7 @@
 #include "config.h"
 
 #ifndef HAVE_INET_ATON
-int inet_aton(const char *cp, struct in_addr *addr)
+int inet_aton(const char *cp, struct in_addr *addr);
 #endif /* HAVE_INET_ATON */
 
 #endif /* _BSD_INET_ATON_H */
