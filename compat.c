9646f3b3f9fa82a6f5cdf49cbb09481517dfb1c6
Ben Lindstrom <mouring@eviladmin.org>
2001-02-16 03:36:46 +0000

    #error no longer required,  dealt with in configure.in now.

--- compat.c
+++ compat.c
@@ -31,11 +31,7 @@ RCSID("$OpenBSD: compat.c,v 1.34 2001/01/21 19:05:48 markus Exp $");
 #  ifdef HAVE_REGEX_H
 #    include <regex.h>
 #  else
-#    ifdef HAVE_REGCOMP
-#      include "fake-regex.h"
-#    else
-#      error "No regular expression library detected.  See INSTALL file."
-#    endif
+#    include "fake-regex.h"
 #  endif
 #endif /* HAVE_LIBPCRE */
 
