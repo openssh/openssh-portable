ddfb1e3a892f450178093964f11cdc534340a56a
Ben Lindstrom <mouring@eviladmin.org>
2001-08-06 22:06:35 +0000

    - jakob@cvs.openbsd.org 2001/08/03 10:31:30      [ssh-add.c ssh-agent.c ssh-keyscan.c]      improve usage(). ok markus@

--- ssh-add.c
+++ ssh-add.c
@@ -35,7 +35,7 @@
  */
 
 #include "includes.h"
-RCSID("$OpenBSD: ssh-add.c,v 1.44 2001/08/01 22:03:33 markus Exp $");
+RCSID("$OpenBSD: ssh-add.c,v 1.45 2001/08/03 10:31:30 jakob Exp $");
 
 #include <openssl/evp.h>
 
@@ -54,6 +54,9 @@ extern char *__progname;
 #else
 char *__progname;
 #endif
+
+/* argv0 */
+extern char *__progname;
 
 /* we keep a cache of one passphrases */
 static char *pass = NULL;
@@ -195,12 +198,16 @@ list_identities(AuthenticationConnection *ac, int do_fp)
 static void
 usage(void)
 {
-	printf("Usage: ssh-add [options]\n");
-	printf("    -l, -L        : list identities\n");
-	printf("    -d            : delete identity\n");
-	printf("    -D            : delete all identities\n");
-	printf("    -s reader_num : add key in the smartcard in reader_num.\n");
-	printf("    -e reader_num : remove key in the smartcard in reader_num.\n");
+	fprintf(stderr, "Usage: %s [options]\n", __progname);
+	fprintf(stderr, "Options:\n");
+	fprintf(stderr, "  -l          List fingerprints of all identities.\n");
+	fprintf(stderr, "  -L          List public key parameters of all identities.\n");
+	fprintf(stderr, "  -d          Delete identity.\n");
+	fprintf(stderr, "  -D          Delete all identities.\n");
+#ifdef SMARTCARD
+	fprintf(stderr, "  -s reader   Add key in smartcard reader.\n");
+	fprintf(stderr, "  -e reader   Remove key in smartcard reader.\n");
+#endif
 }
 
 int
