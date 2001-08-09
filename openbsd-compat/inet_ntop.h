113339e024642f5480efe438d1ae462af356387a
Ben Lindstrom <mouring@eviladmin.org>
2001-08-09 00:56:52 +0000

    - (bal) Minor correction to inet_ntop.h.  _BSD_RRESVPORT_H should be     _BSD_INET_NTOP_H.  Pointed out by Mark Miller <markm@swoon.net>

--- openbsd-compat/inet_ntop.h
+++ openbsd-compat/inet_ntop.h
@@ -1,7 +1,7 @@
-/* $Id: inet_ntop.h,v 1.3 2001/08/07 22:29:10 tim Exp $ */
+/* $Id: inet_ntop.h,v 1.4 2001/08/09 00:56:53 mouring Exp $ */
 
-#ifndef _BSD_RRESVPORT_H
-#define _BSD_RRESVPORT_H
+#ifndef _BSD_INET_NTOP_H
+#define _BSD_INET_NTOP_H
 
 #include "config.h"
 
@@ -10,4 +10,4 @@ const char *
 inet_ntop(int af, const void *src, char *dst, size_t size);
 #endif /* !HAVE_INET_NTOP */
 
-#endif /* _BSD_RRESVPORT_H */
+#endif /* _BSD_INET_NTOP_H */
