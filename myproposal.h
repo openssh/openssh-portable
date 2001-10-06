4c90149c500f8cc7ad26a016ca3f67b586197b84
Ben Lindstrom <mouring@eviladmin.org>
2001-10-06 00:08:09 +0000

    - (bal) AES works under Cray, no more hack.

--- myproposal.h
+++ myproposal.h
@@ -25,17 +25,11 @@
  */
 #define KEX_DEFAULT_KEX		"diffie-hellman-group-exchange-sha1,diffie-hellman-group1-sha1"
 #define	KEX_DEFAULT_PK_ALG	"ssh-rsa,ssh-dss"
-#ifdef _CRAY
-/*Temporary hack until rijndael is ported to CRAY word addressable machines  */
-#define	KEX_DEFAULT_ENCRYPT \
-	"3des-cbc,blowfish-cbc,cast128-cbc,arcfour" 
-#else
 #define	KEX_DEFAULT_ENCRYPT \
 	"aes128-cbc,3des-cbc,blowfish-cbc,cast128-cbc,arcfour," \
 	"aes192-cbc,aes256-cbc," \
 	"rijndael128-cbc,rijndael192-cbc,rijndael256-cbc," \
 	"rijndael-cbc@lysator.liu.se"
-#endif
 #define	KEX_DEFAULT_MAC \
 	"hmac-md5,hmac-sha1,hmac-ripemd160," \
 	"hmac-ripemd160@openssh.com," \
