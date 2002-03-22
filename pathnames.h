7a7edf77ed9e9c982beedb84f2bd384acb7cfcdb
Ben Lindstrom <mouring@eviladmin.org>
2002-03-22 02:42:37 +0000

    - stevesk@cvs.openbsd.org 2002/03/19 03:03:43      [pathnames.h servconf.c servconf.h sshd.c]      _PATH_PRIVSEP_CHROOT_DIR; ok provos@

--- pathnames.h
+++ pathnames.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pathnames.h,v 1.11 2002/02/09 17:37:34 deraadt Exp $	*/
+/*	$OpenBSD: pathnames.h,v 1.12 2002/03/19 03:03:43 stevesk Exp $	*/
 
 /*
  * Author: Tatu Ylonen <ylo@cs.hut.fi>
@@ -139,6 +139,9 @@
 #ifndef _PATH_SFTP_SERVER
 #define _PATH_SFTP_SERVER		"/usr/libexec/sftp-server"
 #endif
+
+/* chroot directory for unprivileged user when UsePrivilegeSeparation=yes */
+#define _PATH_PRIVSEP_CHROOT_DIR	"/var/empty"
 #ifndef _PATH_LS
 #define _PATH_LS			"ls"
 #endif
