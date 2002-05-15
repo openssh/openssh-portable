beecf74e2b3a8cc4dcf00f38b223d5368ca1e0bc
Ben Lindstrom <mouring@eviladmin.org>
2002-05-15 15:59:17 +0000

    - (bal) CVS ID fix up on auth-passwd.c

--- auth-passwd.c
+++ auth-passwd.c
@@ -36,7 +36,7 @@
  */
 
 #include "includes.h"
-RCSID("$OpenBSD: auth-passwd.c,v 1.24 2002/03/04 12:43:06 markus Exp $");
+RCSID("$OpenBSD: auth-passwd.c,v 1.26 2002/05/10 02:30:12 mouring Exp $");
 
 #include "packet.h"
 #include "log.h"
