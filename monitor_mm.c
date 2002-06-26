a95fd3f8ad53d2ac2fb26558e033bd5180131c77
Ben Lindstrom <mouring@eviladmin.org>
2002-06-26 00:22:57 +0000

    - (bal) added back in error check for mmap().  I screwed up, Pointed    out by stevesk@

--- monitor_mm.c
+++ monitor_mm.c
@@ -87,6 +87,8 @@ mm_create(struct mm_master *mmalloc, size_t size)
 #ifdef HAVE_MMAP_ANON_SHARED
 	address = mmap(NULL, size, PROT_WRITE|PROT_READ, MAP_ANON|MAP_SHARED,
 	    -1, 0);
+	if (address == MAP_FAILED)
+		fatal("mmap(%lu): %s", (u_long)size, strerror(errno));
 #else
 	fatal("%s: UsePrivilegeSeparation=yes and Compression=yes not supported",
 	    __func__);
