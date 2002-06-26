f18cd162d310bbd69f272337e8adb57742d322c1
Damien Miller <djm@mindrot.org>
2002-06-26 19:12:59 +1000

    - (djm) setlogin needs pgid==pid on BSD/OS; from itojun@

--- session.c
+++ session.c
@@ -1163,6 +1163,9 @@ do_setusercontext(struct passwd *pw)
 		setpcred(pw->pw_name);
 #endif /* HAVE_SETPCRED */
 #ifdef HAVE_LOGIN_CAP
+#ifdef __bsdi__
+		setpgid(0, 0);
+#endif
 		if (setusercontext(lc, pw, pw->pw_uid,
 		    (LOGIN_SETALL & ~LOGIN_SETPATH)) < 0) {
 			perror("unable to set user context");
