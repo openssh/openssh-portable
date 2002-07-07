8e8ef2a3ffb03d48bb9d6b894152497810f19044
Ben Lindstrom <mouring@eviladmin.org>
2002-07-07 22:14:55 +0000

    - deraadt@cvs.openbsd.org 2002/07/06 01:00:49      [log.c]      KNF

--- log.c
+++ log.c
@@ -34,7 +34,7 @@
  */
 
 #include "includes.h"
-RCSID("$OpenBSD: log.c,v 1.22 2002/02/22 12:20:34 markus Exp $");
+RCSID("$OpenBSD: log.c,v 1.23 2002/07/06 01:00:49 deraadt Exp $");
 
 #include "log.h"
 #include "xmalloc.h"
@@ -92,6 +92,7 @@ SyslogFacility
 log_facility_number(char *name)
 {
 	int i;
+
 	if (name != NULL)
 		for (i = 0; log_facilities[i].name; i++)
 			if (strcasecmp(log_facilities[i].name, name) == 0)
@@ -103,6 +104,7 @@ LogLevel
 log_level_number(char *name)
 {
 	int i;
+
 	if (name != NULL)
 		for (i = 0; log_levels[i].name; i++)
 			if (strcasecmp(log_levels[i].name, name) == 0)
@@ -116,6 +118,7 @@ void
 error(const char *fmt,...)
 {
 	va_list args;
+
 	va_start(args, fmt);
 	do_log(SYSLOG_LEVEL_ERROR, fmt, args);
 	va_end(args);
@@ -127,6 +130,7 @@ void
 log(const char *fmt,...)
 {
 	va_list args;
+
 	va_start(args, fmt);
 	do_log(SYSLOG_LEVEL_INFO, fmt, args);
 	va_end(args);
@@ -138,6 +142,7 @@ void
 verbose(const char *fmt,...)
 {
 	va_list args;
+
 	va_start(args, fmt);
 	do_log(SYSLOG_LEVEL_VERBOSE, fmt, args);
 	va_end(args);
@@ -149,6 +154,7 @@ void
 debug(const char *fmt,...)
 {
 	va_list args;
+
 	va_start(args, fmt);
 	do_log(SYSLOG_LEVEL_DEBUG1, fmt, args);
 	va_end(args);
@@ -158,6 +164,7 @@ void
 debug2(const char *fmt,...)
 {
 	va_list args;
+
 	va_start(args, fmt);
 	do_log(SYSLOG_LEVEL_DEBUG2, fmt, args);
 	va_end(args);
@@ -167,6 +174,7 @@ void
 debug3(const char *fmt,...)
 {
 	va_list args;
+
 	va_start(args, fmt);
 	do_log(SYSLOG_LEVEL_DEBUG3, fmt, args);
 	va_end(args);
