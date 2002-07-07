024f08f3f68060d65cdfe083fe30ef53524c56c0
Ben Lindstrom <mouring@eviladmin.org>
2002-07-07 02:17:36 +0000

    Added author to ports-aix.[ch].

--- openbsd-compat/port-aix.h
+++ openbsd-compat/port-aix.h
@@ -1,4 +1,7 @@
 /*
+ *
+ * Copyright (c) 2001 Gert Doering.  All rights reserved.
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
