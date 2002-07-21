21e04eb2be1e6a621148b29ac6f57f2677dedc21
Kevin Steves <stevesk@pobox.com>
2002-07-21 23:20:07 +0000

    - (stevesk) [auth-pam.h] unneeded include

--- auth-pam.h
+++ auth-pam.h
@@ -1,4 +1,4 @@
-/* $Id: auth-pam.h,v 1.13 2002/07/21 23:16:01 stevesk Exp $ */
+/* $Id: auth-pam.h,v 1.14 2002/07/21 23:20:07 stevesk Exp $ */
 
 /*
  * Copyright (c) 2000 Damien Miller.  All rights reserved.
@@ -26,8 +26,6 @@
 
 #include "includes.h"
 #ifdef USE_PAM
-
-#include <pwd.h> /* For struct passwd */
 
 void start_pam(const char *user);
 void finish_pam(void);
