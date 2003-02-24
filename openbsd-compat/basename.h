17bbc45a11a6cab7f308095c5aef6d9fbfe798c9
Damien Miller <djm@mindrot.org>
2003-02-25 10:25:12 +1100

    me <- idiot

--- openbsd-compat/basename.h
+++ openbsd-compat/basename.h
@@ -1,4 +1,4 @@
-/* $Id: basename.h,v 1.1 2003/02/24 01:55:56 djm Exp $ */
+/* $Id: basename.h,v 1.2 2003/02/24 23:25:12 djm Exp $ */
 
 #ifndef _BASENAME_H 
 #define _BASENAME_H
@@ -6,7 +6,7 @@
 
 #if !defined(HAVE_BASENAME)
 
-char *getcwd(char *pt, size_t size);
+char *basename(char *path);
 
 #endif /* !defined(HAVE_BASENAME) */
 #endif /* _BASENAME_H */
