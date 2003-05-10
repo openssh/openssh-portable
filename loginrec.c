0d8b792931b32442b66b9300e8678e425446ae3b
Damien Miller <djm@mindrot.org>
2003-05-10 23:42:12 +1000

    - (djm) 2-clause license on loginrec.c, with permission from    andre@ae-35.com

--- loginrec.c
+++ loginrec.c
@@ -12,11 +12,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *      This product includes software developed by Markus Friedl.
- * 4. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
@@ -163,7 +158,7 @@
 #include "log.h"
 #include "atomicio.h"
 
-RCSID("$Id: loginrec.c,v 1.50 2003/04/09 11:12:52 djm Exp $");
+RCSID("$Id: loginrec.c,v 1.51 2003/05/10 13:42:12 djm Exp $");
 
 #ifdef HAVE_UTIL_H
 #  include <util.h>
