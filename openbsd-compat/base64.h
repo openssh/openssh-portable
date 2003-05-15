ffda4cb2181683af35a4aee85c00d83ed9b7f11c
Damien Miller <djm@mindrot.org>
2003-05-15 13:57:51 +1000

    - (djm) Avoid uuencode.c warnings

--- openbsd-compat/base64.h
+++ openbsd-compat/base64.h
@@ -1,4 +1,4 @@
-/* $Id: base64.h,v 1.4 2003/02/24 04:45:43 djm Exp $ */
+/* $Id: base64.h,v 1.5 2003/05/15 03:57:51 djm Exp $ */
 
 #ifndef _BSD_BASE64_H
 #define _BSD_BASE64_H
@@ -10,14 +10,14 @@
 int b64_ntop(u_char const *src, size_t srclength, char *target, 
     size_t targsize);
 # endif /* !HAVE_B64_NTOP */
-# define __b64_ntop b64_ntop
+# define __b64_ntop(a,b,c,d) b64_ntop(a,b,c,d)
 #endif /* HAVE___B64_NTOP */
 
 #ifndef HAVE___B64_PTON
 # ifndef HAVE_B64_PTON
 int b64_pton(char const *src, u_char *target, size_t targsize);
 # endif /* !HAVE_B64_PTON */
-# define __b64_pton b64_pton
+# define __b64_pton(a,b,c) b64_pton(a,b,c)
 #endif /* HAVE___B64_PTON */
 
 #endif /* _BSD_BASE64_H */
