ceb3136f5f121e0d6d3a78dcc52c6bf186a7f872
Damien Miller <djm@mindrot.org>
2003-06-05 09:56:35 +1000

    KNF

--- openbsd-compat/fake-socket.h
+++ openbsd-compat/fake-socket.h
@@ -1,4 +1,4 @@
-/* $Id: fake-socket.h,v 1.3 2002/04/12 03:35:40 tim Exp $ */
+/* $Id: fake-socket.h,v 1.4 2003/06/04 23:56:35 djm Exp $ */
 
 #ifndef _FAKE_SOCKET_H
 #define _FAKE_SOCKET_H
@@ -7,11 +7,12 @@
 #include "sys/types.h"
 
 #ifndef HAVE_STRUCT_SOCKADDR_STORAGE
+
 # define	_SS_MAXSIZE	128	/* Implementation specific max size */
 # define       _SS_PADSIZE     (_SS_MAXSIZE - sizeof (struct sockaddr))
 
 struct sockaddr_storage {
-	struct	sockaddr ss_sa;
+	struct sockaddr	ss_sa;
 	char		__ss_pad2[_SS_PADSIZE];
 };
 # define ss_family ss_sa.sa_family
@@ -19,22 +20,22 @@ struct sockaddr_storage {
 
 #ifndef IN6_IS_ADDR_LOOPBACK
 # define IN6_IS_ADDR_LOOPBACK(a) \
-	(((u_int32_t *) (a))[0] == 0 && ((u_int32_t *) (a))[1] == 0 && \
-	 ((u_int32_t *) (a))[2] == 0 && ((u_int32_t *) (a))[3] == htonl (1))
+	(((u_int32_t *)(a))[0] == 0 && ((u_int32_t *)(a))[1] == 0 && \
+	 ((u_int32_t *)(a))[2] == 0 && ((u_int32_t *)(a))[3] == htonl(1))
 #endif /* !IN6_IS_ADDR_LOOPBACK */
 
 #ifndef HAVE_STRUCT_IN6_ADDR
 struct in6_addr {
-	u_int8_t		s6_addr[16];
+	u_int8_t	s6_addr[16];
 };
 #endif /* !HAVE_STRUCT_IN6_ADDR */
 
 #ifndef HAVE_STRUCT_SOCKADDR_IN6
 struct sockaddr_in6 {
-   unsigned short sin6_family;
-	u_int16_t sin6_port;
-	u_int32_t sin6_flowinfo;
-	struct in6_addr sin6_addr;
+	unsigned short	sin6_family;
+	u_int16_t	sin6_port;
+	u_int32_t	sin6_flowinfo;
+	struct in6_addr	sin6_addr;
 };
 #endif /* !HAVE_STRUCT_SOCKADDR_IN6 */
 
