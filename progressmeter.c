2b92d32e199aa93f2e9d585ca00d56c46775c177
Damien Miller <djm@mindrot.org>
2003-06-11 22:05:06 +1000

    - deraadt@cvs.openbsd.org 2003/06/10 22:20:52      [packet.c progressmeter.c]      mostly ansi cleanup; pval ok

--- progressmeter.c
+++ progressmeter.c
@@ -64,7 +64,7 @@
  */
 
 #include "includes.h"
-RCSID("$OpenBSD: progressmeter.c,v 1.6 2003/04/07 21:58:05 millert Exp $");
+RCSID("$OpenBSD: progressmeter.c,v 1.7 2003/06/10 22:20:52 deraadt Exp $");
 
 #ifdef HAVE_LIBGEN_H
 #include <libgen.h>
@@ -127,7 +127,7 @@ start_progress_meter(char *file, off_t filesize, off_t *counter)
 }
 
 void
-stop_progress_meter()
+stop_progress_meter(void)
 {
 	alarm(0);
 	draw_progress_meter();
@@ -165,7 +165,7 @@ foregroundproc(void)
 }
 
 static void
-draw_progress_meter()
+draw_progress_meter(void)
 {
 	static const char spaces[] = "                          "
 	    "                                                   "
