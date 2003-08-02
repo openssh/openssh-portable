048737c9bcf25a02932d17b74398082930ee817e
Darren Tucker <dtucker@zip.com.au>
2003-08-02 23:33:48 +1000

    Remove unused variable.

--- openbsd-compat/bsd-misc.c
+++ openbsd-compat/bsd-misc.c
@@ -25,7 +25,7 @@
 #include "includes.h"
 #include "xmalloc.h"
 
-RCSID("$Id: bsd-misc.c,v 1.14 2003/08/02 13:31:42 dtucker Exp $");
+RCSID("$Id: bsd-misc.c,v 1.15 2003/08/02 13:33:48 dtucker Exp $");
 
 /*
  * NB. duplicate __progname in case it is an alias for argv[0]
@@ -171,7 +171,7 @@ int nanosleep(const struct timespec *req, struct timespec *rem)
 pid_t
 tcgetpgrp(int fd)
 {
-	int result, ctty_pgrp;
+	int ctty_pgrp;
 
 	if (ioctl(STDOUT_FILENO, TIOCGPGRP, &ctty_pgrp) == -1)
 		return(-1);
