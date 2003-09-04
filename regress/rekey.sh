60de9f1851f19b43ba97a8d05861a4bf3416703d
Darren Tucker <dtucker@zip.com.au>
2003-09-04 14:02:56 +1000

    - [regress/rekey.sh] Remove dependence on /dev/zero (not all platforms have      it).  The sparse file will take less disk space too.

--- regress/rekey.sh
+++ regress/rekey.sh
@@ -8,7 +8,7 @@ COPY=${OBJ}/copy
 LOG=${OBJ}/log
 
 rm -f ${COPY} ${LOG} ${DATA}
-dd if=/dev/zero of=${DATA} bs=1k count=512 > /dev/null 2>&1
+dd if=/bin/ls${EXEEXT} of=${DATA} bs=1k seek=511 count=1 > /dev/null 2>&1
 
 for s in 16 1k 128k 256k; do
 	trace "rekeylimit ${s}"
