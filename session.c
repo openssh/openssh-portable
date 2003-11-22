ce34674a9fe98fd8d9df6f54de8dd3fc707826a8
Damien Miller <djm@mindrot.org>
2003-11-22 14:41:58 +1100

    sync whitespace - no code change

--- session.c
+++ session.c
@@ -204,7 +204,6 @@ do_authenticated(Authctxt *authctxt)
 		close(startup_pipe);
 		startup_pipe = -1;
 	}
-
 	/* setup the channel layer */
 	if (!no_port_forwarding_flag && options.allow_tcp_forwarding)
 		channel_permit_all_opens();
