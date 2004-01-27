b2d1c2b3b82fbbd78ed3dce8e26a3e70fb1b9165
Damien Miller <djm@mindrot.org>
2004-01-27 21:20:59 +1100

    - hshoexer@cvs.openbsd.org 2004/01/23 19:26:33      [cipher.c]      rename acss@opebsd.org to acss@openssh.org      ok deraadt@

--- cipher.c
+++ cipher.c
@@ -35,7 +35,7 @@
  */
 
 #include "includes.h"
-RCSID("$OpenBSD: cipher.c,v 1.67 2004/01/23 17:06:03 hshoexer Exp $");
+RCSID("$OpenBSD: cipher.c,v 1.68 2004/01/23 19:26:33 hshoexer Exp $");
 
 #include "xmalloc.h"
 #include "log.h"
@@ -104,7 +104,7 @@ struct Cipher {
 	{ "aes256-ctr", 	SSH_CIPHER_SSH2, 16, 32, evp_aes_128_ctr },
 #endif
 #if defined(EVP_CTRL_SET_ACSS_MODE)
-	{ "acss@openbsd.org",	SSH_CIPHER_SSH2, 16, 5, EVP_acss },
+	{ "acss@openssh.org",	SSH_CIPHER_SSH2, 16, 5, EVP_acss },
 #endif
 	{ NULL,			SSH_CIPHER_ILLEGAL, 0, 0, NULL }
 };
