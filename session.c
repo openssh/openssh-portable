33370e0287649c135eef8e7a0e22aee754deeac5
Darren Tucker <dtucker@zip.com.au>
2005-02-09 22:17:28 +1100

    - (dtucker) [configure.ac session.c] Some platforms (eg some SCO) require    the username to be passed to the passwd command when changing expired    passwords.  ok djm@

--- session.c
+++ session.c
@@ -1346,7 +1346,12 @@ do_pwchange(Session *s)
 	if (s->ttyfd != -1) {
 		fprintf(stderr,
 		    "You must change your password now and login again!\n");
+#ifdef PASSWD_NEEDS_USERNAME
+		execl(_PATH_PASSWD_PROG, "passwd", s->pw->pw_name,
+		    (char *)NULL);
+#else
 		execl(_PATH_PASSWD_PROG, "passwd", (char *)NULL);
+#endif
 		perror("passwd");
 	} else {
 		fprintf(stderr,
