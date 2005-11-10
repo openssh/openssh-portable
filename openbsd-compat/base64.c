30d6974124fd54c5bd9f101d9e31291fc9eda1b9
Darren Tucker <dtucker@zip.com.au>
2005-11-10 19:29:12 +1100

    - (dtucker) [openbsd-compat/sigact.c] Update from OpenBSD 1.3 -> 1.4.    Id and copyright sync only, there were no substantial changes we need.

--- openbsd-compat/base64.c
+++ openbsd-compat/base64.c
@@ -139,7 +139,7 @@ b64_ntop(u_char const *src, size_t srclength, char *target, size_t targsize)
 	size_t datalength = 0;
 	u_char input[3];
 	u_char output[4];
-	int i;
+	u_int i;
 
 	while (2 < srclength) {
 		input[0] = *src++;
@@ -206,7 +206,8 @@ b64_ntop(u_char const *src, size_t srclength, char *target, size_t targsize)
 int
 b64_pton(char const *src, u_char *target, size_t targsize)
 {
-	int tarindex, state, ch;
+	u_int tarindex, state;
+	int ch;
 	char *pos;
 
 	state = 0;
--- openbsd-compat/sigact.c
+++ openbsd-compat/sigact.c
@@ -1,7 +1,7 @@
-/*	$OpenBSD: sigaction.c,v 1.3 1999/06/27 08:14:21 millert Exp $	*/
+/*	$OpenBSD: sigaction.c,v 1.4 2001/01/22 18:01:48 millert Exp $	*/
 
 /****************************************************************************
- * Copyright (c) 1998 Free Software Foundation, Inc.                        *
+ * Copyright (c) 1998,2000 Free Software Foundation, Inc.                   *
  *                                                                          *
  * Permission is hereby granted, free of charge, to any person obtaining a  *
  * copy of this software and associated documentation files (the            *
