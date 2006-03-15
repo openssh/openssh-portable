b24c2f8e33781be43702d8cfc69d4fbd0547e638
Damien Miller <djm@mindrot.org>
2006-03-15 12:04:36 +1100

    - djm@cvs.openbsd.org 2006/03/13 08:16:00      [sshd.c]      don't log that we are listening on a socket before the listen() call      actually succeeds, bz #1162 reported by Senthil Kumar; ok dtucker@

--- sshd.c
+++ sshd.c
@@ -42,7 +42,7 @@
  */
 
 #include "includes.h"
-RCSID("$OpenBSD: sshd.c,v 1.323 2006/02/20 17:19:54 stevesk Exp $");
+RCSID("$OpenBSD: sshd.c,v 1.325 2006/03/13 08:16:00 djm Exp $");
 
 #include <sys/types.h>
 #ifdef HAVE_SYS_STAT_H
@@ -1333,10 +1333,10 @@ main(int ac, char **av)
 			num_listen_socks++;
 
 			/* Start listening on the port. */
-			logit("Server listening on %s port %s.", ntop, strport);
 			if (listen(listen_sock, SSH_LISTEN_BACKLOG) < 0)
-				fatal("listen: %.100s", strerror(errno));
-
+				fatal("listen on [%s]:%s: %.100s",
+				    ntop, strport, strerror(errno));
+			logit("Server listening on %s port %s.", ntop, strport);
 		}
 		freeaddrinfo(options.listen_addrs);
 
