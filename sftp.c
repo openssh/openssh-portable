a5362458d0e1d2387a05f5c7c29e4f5b87501824
Darren Tucker <dtucker@zip.com.au>
2006-07-12 22:07:08 +1000

    - stevesk@cvs.openbsd.org 2006/07/10 16:01:57      [sftp-glob.c sftp-common.h sftp.c]      buffer.h only needed in sftp-common.h and remove some unneeded      user includes; ok djm@

--- sftp.c
+++ sftp.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: sftp.c,v 1.83 2006/07/08 21:47:12 stevesk Exp $ */
+/* $OpenBSD: sftp.c,v 1.84 2006/07/10 16:01:57 stevesk Exp $ */
 /*
  * Copyright (c) 2001-2004 Damien Miller <djm@openbsd.org>
  *
@@ -35,7 +35,6 @@ typedef void EditLine;
 #endif
 #include <signal.h>
 
-#include "buffer.h"
 #include "xmalloc.h"
 #include "log.h"
 #include "pathnames.h"
