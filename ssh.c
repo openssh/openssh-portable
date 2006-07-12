ba724050263c0bca0a7dffa26462d046c4df7e01
Darren Tucker <dtucker@zip.com.au>
2006-07-12 22:24:22 +1000

    - stevesk@cvs.openbsd.org 2006/07/11 20:27:56      [authfile.c ssh.c]      need <errno.h> here also (it's also included in <openssl/err.h>)

--- ssh.c
+++ ssh.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: ssh.c,v 1.284 2006/07/11 20:16:43 stevesk Exp $ */
+/* $OpenBSD: ssh.c,v 1.285 2006/07/11 20:27:56 stevesk Exp $ */
 /*
  * Author: Tatu Ylonen <ylo@cs.hut.fi>
  * Copyright (c) 1995 Tatu Ylonen <ylo@cs.hut.fi>, Espoo, Finland
@@ -52,6 +52,7 @@
 #include <sys/un.h>
 
 #include <ctype.h>
+#include <errno.h>
 #include <fcntl.h>
 #ifdef HAVE_PATHS_H
 #include <paths.h>
