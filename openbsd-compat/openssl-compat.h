a2ed75582fb10a67424bca64775d0121631760fb
Darren Tucker <dtucker@zip.com.au>
2007-06-14 23:38:39 +1000

    - (dtucker) [openbsd-compat/openssl-compat.h] Merge USE_BUILTIN_RIJNDAEL    sections.  Fixes builds with early OpenSSL 0.9.6 versions.

--- openbsd-compat/openssl-compat.h
+++ openbsd-compat/openssl-compat.h
@@ -1,4 +1,4 @@
-/* $Id: openssl-compat.h,v 1.8 2007/06/14 13:21:33 dtucker Exp $ */
+/* $Id: openssl-compat.h,v 1.9 2007/06/14 13:38:39 dtucker Exp $ */
 
 /*
  * Copyright (c) 2005 Darren Tucker <dtucker@zip.com.au>
@@ -25,19 +25,16 @@
 # define USE_BUILTIN_RIJNDAEL
 #endif
 
+#if (OPENSSL_VERSION_NUMBER < 0x00907000L) || defined(OPENSSL_LOBOTOMISED_AES)
+# define USE_BUILTIN_RIJNDAEL
+#endif
+
 #ifdef USE_BUILTIN_RIJNDAEL
 # include "rijndael.h"
 # define AES_KEY rijndael_ctx
 # define AES_BLOCK_SIZE 16
 # define AES_encrypt(a, b, c)		rijndael_encrypt(c, a, b)
 # define AES_set_encrypt_key(a, b, c)	rijndael_set_key(c, (char *)a, b, 1)
-#endif
-
-#if (OPENSSL_VERSION_NUMBER < 0x00907000L) || defined(OPENSSL_LOBOTOMISED_AES)
-# define USE_BUILTIN_RIJNDAEL
-#endif
-
-#ifdef USE_BUILTIN_RIJNDAEL
 # define EVP_aes_128_cbc evp_rijndael
 # define EVP_aes_192_cbc evp_rijndael
 # define EVP_aes_256_cbc evp_rijndael
