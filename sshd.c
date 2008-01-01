5891116cb328acbca829be47d3cd36a3bddaa85b
Darren Tucker <dtucker@zip.com.au>
2008-01-01 20:33:09 +1100

    - dtucker@cvs.openbsd.org 2007/12/31 15:27:04      [sshd.c]      When in inetd mode, have sshd generate a Protocol 1 ephemeral server      key only for connections where the client chooses Protocol 1 as opposed      to when it's enabled in the server's config.  Speeds up Protocol 2      connections to inetd-mode servers that also allow Protocol 1.  bz #440,      based on a patch from bruno at wolff.to, ok markus@

--- sshd.c
+++ sshd.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: sshd.c,v 1.352 2007/12/27 14:22:08 dtucker Exp $ */
+/* $OpenBSD: sshd.c,v 1.353 2007/12/31 15:27:04 dtucker Exp $ */
 /*
  * Author: Tatu Ylonen <ylo@cs.hut.fi>
  * Copyright (c) 1995 Tatu Ylonen <ylo@cs.hut.fi>, Espoo, Finland
@@ -1599,10 +1599,6 @@ main(int ac, char **av)
 	/* Get a connection, either from inetd or a listening TCP socket */
 	if (inetd_flag) {
 		server_accept_inetd(&sock_in, &sock_out);
-
-		if ((options.protocol & SSH_PROTO_1) &&
-		    sensitive_data.server_key == NULL)
-			generate_ephemeral_server_key();
 	} else {
 		server_listen();
 
@@ -1771,6 +1767,10 @@ main(int ac, char **av)
 		alarm(options.login_grace_time);
 
 	sshd_exchange_identification(sock_in, sock_out);
+
+	/* In inetd mode, generate ephemeral key only for proto 1 connections */
+	if (!compat20 && inetd_flag && sensitive_data.server_key == NULL)
+		generate_ephemeral_server_key();
 
 	packet_set_nonblocking();
 
