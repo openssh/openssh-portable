02db85bdcbb07f632cddd927ded208f3d4f1e65f
Tim Rice <tim@multitalents.net>
2008-03-14 11:02:51 -0700

    - (tim) [regress/localcommand.sh] Shell portability fix. Reported by imorgan at    nas.nasa.gov

--- regress/localcommand.sh
+++ regress/localcommand.sh
@@ -8,7 +8,7 @@ echo 'LocalCommand echo foo' >> $OBJ/ssh_proxy
 
 for p in 1 2; do
 	verbose "test $tid: proto $p localcommand"
-	a=$(${SSH} -F $OBJ/ssh_proxy -$p somehost true)
+	a=`${SSH} -F $OBJ/ssh_proxy -$p somehost true`
 	if [ "$a" != "foo" ] ; then
 		fail "$tid proto $p"
 	fi
