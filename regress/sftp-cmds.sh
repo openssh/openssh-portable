51430434098ff74055925423787866f599e75177
Tim Rice <tim@multitalents.net>
2008-03-14 10:39:17 -0700

    - (tim) [regress/sftp-cmds.sh] s/cd/lcd/ in lls test. Reported by    vinschen at redhat.com. Add () to put echo commands in subshell for lls test    I mistakenly left out of last commit.

--- regress/sftp-cmds.sh
+++ regress/sftp-cmds.sh
@@ -44,7 +44,7 @@ rm -rf ${COPY} ${COPY}.1 ${COPY}.2 ${COPY}.dd ${COPY}.dd2
 mkdir ${COPY}.dd
 
 verbose "$tid: lls"
-echo "cd ${OBJ}" ; echo "lls" | ${SFTP} -P ${SFTPSERVER} 2>&1 | \
+(echo "lcd ${OBJ}" ; echo "lls") | ${SFTP} -P ${SFTPSERVER} 2>&1 | \
 	grep copy.dd >/dev/null 2>&1 || fail "lls failed"
 
 verbose "$tid: lls w/path"
