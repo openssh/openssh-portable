2ff24f66bf5a955bd8beec5dcd1badab04b7ef66
Damien Miller <djm@mindrot.org>
2008-03-27 11:18:13 +1100

    - djm@cvs.openbsd.org 2008/03/27 00:16:49      [version.h]      openssh-4.9

--- version.h
+++ version.h
@@ -1,6 +1,6 @@
-/* $OpenBSD: version.h,v 1.51 2008/03/04 21:15:42 deraadt Exp $ */
+/* $OpenBSD: version.h,v 1.52 2008/03/27 00:16:49 djm Exp $ */
 
-#define SSH_VERSION	"OpenSSH_4.8"
+#define SSH_VERSION	"OpenSSH_4.9"
 
 #define SSH_PORTABLE	"p1"
 #define SSH_RELEASE	SSH_VERSION SSH_PORTABLE
