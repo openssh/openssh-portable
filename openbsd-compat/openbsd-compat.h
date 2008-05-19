a4be7c23fdcf8a1da5420068dc4bd4db45af9c9c
Damien Miller <djm@mindrot.org>
2008-05-19 14:47:37 +1000

    - (djm) [openbsd-compat/bsd-arc4random.c openbsd-compat/openbsd-compat.c]      [configure.ac] Implement arc4random_buf(), import implementation of      arc4random_uniform() from OpenBSD

--- openbsd-compat/openbsd-compat.h
+++ openbsd-compat/openbsd-compat.h
@@ -1,4 +1,4 @@
-/* $Id: openbsd-compat.h,v 1.43 2007/06/25 12:15:13 dtucker Exp $ */
+/* $Id: openbsd-compat.h,v 1.44 2008/05/19 04:47:37 djm Exp $ */
 
 /*
  * Copyright (c) 1999-2003 Damien Miller.  All rights reserved.
@@ -150,6 +150,14 @@ int getpeereid(int , uid_t *, gid_t *);
 unsigned int arc4random(void);
 void arc4random_stir(void);
 #endif /* !HAVE_ARC4RANDOM */
+
+#ifndef HAVE_ARC4RANDOM_BUF
+void arc4random_buf(void *, size_t);
+#endif
+
+#ifndef HAVE_ARC4RANDOM_UNIFORM
+u_int32_t arc4random_uniform(u_int32_t);
+#endif
 
 #ifndef HAVE_ASPRINTF
 int asprintf(char **, const char *, ...);
