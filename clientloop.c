586b00532f89e6166e6a74bdf3e801cc59449b84
Damien Miller <djm@mindrot.org>
2008-12-09 14:11:32 +1100

    - djm@cvs.openbsd.org 2008/12/09 02:38:18      [clientloop.c]      The ~C escape handler does not work correctly for multiplexed sessions -      it opens a commandline on the master session, instead of on the slave      that requested it. Disable it on slave sessions until such time as it      is fixed; bz#1543 report from Adrian Bridgett via Colin Watson      ok markus@

--- clientloop.c
+++ clientloop.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: clientloop.c,v 1.205 2008/12/02 19:01:07 markus Exp $ */
+/* $OpenBSD: clientloop.c,v 1.206 2008/12/09 02:38:18 djm Exp $ */
 /*
  * Author: Tatu Ylonen <ylo@cs.hut.fi>
  * Copyright (c) 1995 Tatu Ylonen <ylo@cs.hut.fi>, Espoo, Finland
@@ -1042,7 +1042,6 @@ process_escapes(Channel *c, Buffer *bin, Buffer *bout, Buffer *berr,
 Supported escape sequences:\r\n\
   %c.  - terminate session\r\n\
   %cB  - send a BREAK to the remote system\r\n\
-  %cC  - open a command line\r\n\
   %cR  - Request rekey (SSH protocol 2 only)\r\n\
   %c#  - list forwarded connections\r\n\
   %c?  - this message\r\n\
@@ -1051,8 +1050,7 @@ Supported escape sequences:\r\n\
 					    escape_char, escape_char,
 					    escape_char, escape_char,
 					    escape_char, escape_char,
-					    escape_char, escape_char,
-					    escape_char);
+					    escape_char, escape_char);
 				} else {
 					snprintf(string, sizeof string,
 "%c?\r\n\
@@ -1087,6 +1085,8 @@ Supported escape sequences:\r\n\
 				continue;
 
 			case 'C':
+				if (c && c->ctl_fd != -1)
+					goto noescape;
 				process_cmdline();
 				continue;
 
