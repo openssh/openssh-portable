1aed65eb27feec505997c98621bdf158f9ab8b99
Damien Miller <djm@mindrot.org>
2010-03-04 21:53:35 +1100

    - djm@cvs.openbsd.org 2010/03/04 10:36:03      [auth-rh-rsa.c auth-rsa.c auth.c auth.h auth2-hostbased.c auth2-pubkey.c]      [authfile.c authfile.h hostfile.c hostfile.h servconf.c servconf.h]      [ssh-keygen.c ssh.1 sshconnect.c sshd_config.5]      Add a TrustedUserCAKeys option to sshd_config to specify CA keys that      are trusted to authenticate users (in addition than doing it per-user      in authorized_keys).
         Add a RevokedKeys option to sshd_config and a @revoked marker to
         known_hosts to allow keys to me revoked and banned for user or host
         authentication.
    
         feedback and ok markus@

--- ssh-keygen.c
+++ ssh-keygen.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: ssh-keygen.c,v 1.180 2010/03/02 23:20:57 djm Exp $ */
+/* $OpenBSD: ssh-keygen.c,v 1.181 2010/03/04 10:36:03 djm Exp $ */
 /*
  * Author: Tatu Ylonen <ylo@cs.hut.fi>
  * Copyright (c) 1994 Tatu Ylonen <ylo@cs.hut.fi>, Espoo, Finland
