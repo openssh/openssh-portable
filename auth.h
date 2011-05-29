d8478b6a9b32760d47c2419279c4a73f5f88fdb6
Damien Miller <djm@mindrot.org>
2011-05-29 21:39:36 +1000

    OpenBSD CVS Sync    - djm@cvs.openbsd.org 2011/05/23 03:30:07      [auth-rsa.c auth.c auth.h auth2-pubkey.c monitor.c monitor_wrap.c pathnames.h servconf.c servconf.h sshd.8 sshd_config sshd_config.5]      allow AuthorizedKeysFile to specify multiple files, separated by spaces.      Bring back authorized_keys2 as a default search path (to avoid breaking      existing users of this file), but override this in sshd_config so it will      be no longer used on fresh installs. Maybe in 2015 we can remove it      entierly :)
         feedback and ok markus@ dtucker@

--- auth.h
+++ auth.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: auth.h,v 1.68 2011/05/11 04:47:06 djm Exp $ */
+/* $OpenBSD: auth.h,v 1.69 2011/05/23 03:30:07 djm Exp $ */
 
 /*
  * Copyright (c) 2000 Markus Friedl.  All rights reserved.
@@ -168,7 +168,7 @@ char	*get_challenge(Authctxt *);
 int	verify_response(Authctxt *, const char *);
 void	abandon_challenge_response(Authctxt *);
 
-char	*authorized_keys_file(struct passwd *);
+char	*expand_authorized_keys(const char *, struct passwd *pw);
 char	*authorized_principals_file(struct passwd *);
 
 FILE	*auth_openkeyfile(const char *, struct passwd *, int);
