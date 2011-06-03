dd9e0385abcf9dd2e6c9fe6dc082b0ffd4ae2bac
Darren Tucker <dtucker@zip.com.au>
2011-06-03 11:17:52 +1000

    Remove the !HAVE_SOCKETPAIR case.  We use socketpair unconditionally in other places and the survey data we have does not show any systems that use it. "nuke it" djm@

--- monitor.c
+++ monitor.c
@@ -1853,13 +1853,8 @@ mm_init_compression(struct mm_master *mm)
 static void
 monitor_socketpair(int *pair)
 {
-#ifdef HAVE_SOCKETPAIR
 	if (socketpair(AF_UNIX, SOCK_STREAM, 0, pair) == -1)
 		fatal("%s: socketpair", __func__);
-#else
-	fatal("%s: UsePrivilegeSeparation=yes not supported",
-	    __func__);
-#endif
 	FD_CLOSEONEXEC(pair[0]);
 	FD_CLOSEONEXEC(pair[1]);
 }
