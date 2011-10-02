68afb8c5f242ec74f48fd86137122399435dd757
Darren Tucker <dtucker@zip.com.au>
2011-10-02 18:59:03 +1100

    - markus@cvs.openbsd.org 2011/09/23 07:45:05      [mux.c readconf.h channels.h compat.h compat.c ssh.c readconf.c channels.c     version.h]      unbreak remote portforwarding with dynamic allocated listen ports:      1) send the actual listen port in the open message (instead of 0).         this allows multiple forwardings with a dynamic listen port      2) update the matching permit-open entry, so we can identify where         to connect to      report: den at skbkontur.ru and P. Szczygielski      feedback and ok djm@

--- version.h
+++ version.h
@@ -1,6 +1,6 @@
-/* $OpenBSD: version.h,v 1.62 2011/08/02 23:13:01 djm Exp $ */
+/* $OpenBSD: version.h,v 1.63 2011/09/23 07:45:05 markus Exp $ */
 
-#define SSH_VERSION	"OpenSSH_5.9"
+#define SSH_VERSION	"OpenSSH_6.0-beta"
 
 #define SSH_PORTABLE	"p1"
 #define SSH_RELEASE	SSH_VERSION SSH_PORTABLE
