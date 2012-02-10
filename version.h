db854559be225497fd43fe4a1ff93356204567f3
Damien Miller <djm@mindrot.org>
2012-02-11 08:19:44 +1100

    - markus@cvs.openbsd.org 2012/02/09 20:00:18      [version.h]      move from 6.0-beta to 6.0

--- version.h
+++ version.h
@@ -1,6 +1,6 @@
-/* $OpenBSD: version.h,v 1.63 2011/09/23 07:45:05 markus Exp $ */
+/* $OpenBSD: version.h,v 1.64 2012/02/09 20:00:18 markus Exp $ */
 
-#define SSH_VERSION	"OpenSSH_6.0-beta"
+#define SSH_VERSION	"OpenSSH_6.0"
 
 #define SSH_PORTABLE	"p1"
 #define SSH_RELEASE	SSH_VERSION SSH_PORTABLE
