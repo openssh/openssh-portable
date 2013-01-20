d60b2108307dccdc596d1bf3195f6882067e2b2f
Damien Miller <djm@mindrot.org>
2013-01-20 22:49:58 +1100

    - (djm) [openbsd-compat/sys-tree.h] Sync with OpenBSD. krl.c needs newer     version.

--- krl.c
+++ krl.c
@@ -20,8 +20,8 @@
 
 #include <sys/types.h>
 #include <sys/param.h>
-#include <sys/tree.h>
-#include <sys/queue.h>
+#include <openbsd-compat/sys-tree.h>
+#include <openbsd-compat/sys-queue.h>
 
 #include <errno.h>
 #include <fcntl.h>
