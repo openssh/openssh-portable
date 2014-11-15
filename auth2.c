cbd5126431a181edeb9364f1b142ba98ae2a126b
Chris Rapier <rapier@psc.edu>
2014-11-15 15:20:30 -0500

    extended server logging
    This patch provides enhanced logging capabilities to the server process.
    Information provided includes ciphers used, throughput, bytes transferred,
    and the like. In order to use this patch you *must* direct syslogd to use
    an additional logging socket. This socket will be located in the sshd
    chroot, typically /var/empty. As such you will need to create a
    /var/empty/dev directory and add '-a /var/empty/dev/log' to your syslogd
    configuration.

--- auth2.c
+++ auth2.c
@@ -73,6 +73,8 @@ extern Authmethod method_hostbased;
 extern Authmethod method_gssapi;
 #endif
 
+static int log_flag = 0;
+
 Authmethod *authmethods[] = {
 	&method_none,
 	&method_pubkey,
@@ -225,6 +227,11 @@ input_userauth_request(int type, u_int32_t seq, void *ctxt)
 	service = packet_get_cstring(NULL);
 	method = packet_get_cstring(NULL);
 	debug("userauth-request for user %s service %s method %s", user, service, method);
+	if (!log_flag) {
+		logit("SSH: Server;Ltype: Authname;Remote: %s-%d;Name: %s",
+		      ssh_remote_ipaddr(active_state), ssh_remote_port(active_state), user);
+		log_flag = 1;
+	}
 	debug("attempt %d failures %d", authctxt->attempt, authctxt->failures);
 
 	if ((style = strchr(user, ':')) != NULL)
