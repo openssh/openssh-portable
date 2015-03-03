bbffb23daa0b002dd9f296e396a9ab8a5866b339
Damien Miller <djm@mindrot.org>
2015-03-03 13:50:27 -0800

    more --without-ssh1 fixes

--- opacket.c
+++ opacket.c
@@ -74,7 +74,7 @@ ssh_packet_put_raw(struct ssh *ssh, const void *buf, u_int len)
 		fatal("%s: %s", __func__, ssh_err(r));
 }
 
-#ifdef WITH_OPENSSL
+#ifdef WITH_SSH1
 void
 ssh_packet_put_bignum(struct ssh *ssh, BIGNUM * value)
 {
@@ -83,7 +83,9 @@ ssh_packet_put_bignum(struct ssh *ssh, BIGNUM * value)
 	if ((r = sshpkt_put_bignum1(ssh, value)) != 0)
 		fatal("%s: %s", __func__, ssh_err(r));
 }
+#endif
 
+#ifdef WITH_OPENSSL
 void
 ssh_packet_put_bignum2(struct ssh *ssh, BIGNUM * value)
 {
@@ -148,7 +150,7 @@ ssh_packet_get_int64(struct ssh *ssh)
 	return val;
 }
 
-#ifdef WITH_OPENSSL
+#ifdef WITH_SSH1
 void
 ssh_packet_get_bignum(struct ssh *ssh, BIGNUM * value)
 {
@@ -157,7 +159,9 @@ ssh_packet_get_bignum(struct ssh *ssh, BIGNUM * value)
 	if ((r = sshpkt_get_bignum1(ssh, value)) != 0)
 		fatal("%s: %s", __func__, ssh_err(r));
 }
+#endif
 
+#ifdef WITH_OPENSSL
 void
 ssh_packet_get_bignum2(struct ssh *ssh, BIGNUM * value)
 {
