c7fe79ed7db427f1474e72b9f8b465901d61d3f6
Damien Miller <djm@mindrot.org>
2015-03-20 09:11:59 +1100

    remove error() accidentally inserted for debugging
    pointed out by Christian Hesse

--- monitor_wrap.c
+++ monitor_wrap.c
@@ -153,10 +153,8 @@ mm_request_receive(int sock, Buffer *m)
 	debug3("%s entering", __func__);
 
 	if (atomicio(read, sock, buf, sizeof(buf)) != sizeof(buf)) {
-		if (errno == EPIPE) {
-			error("%s: socket closed", __func__);
+		if (errno == EPIPE)
 			cleanup_exit(255);
-		}
 		fatal("%s: read: %s", __func__, strerror(errno));
 	}
 	msg_len = get_u32(buf);
