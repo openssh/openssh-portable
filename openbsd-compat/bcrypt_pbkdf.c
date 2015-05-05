7ef9b2bc4eb1e5c40109423fe42f857585afeefe
Andreas Schneider <asn@cryptomilk.org>
2015-05-05 10:13:51 +0200

    bcrypt_pbkdf: Fix out-of-bounds read
    We are overrunning the array 'out' of 32 bytes at byte offset 1022 using
    index i.
    
    Signed-off-by: Andreas Schneider <asn@cryptomilk.org>

--- openbsd-compat/bcrypt_pbkdf.c
+++ openbsd-compat/bcrypt_pbkdf.c
@@ -161,7 +161,7 @@ bcrypt_pbkdf(const char *pass, size_t passlen, const u_int8_t *salt, size_t salt
 		amt = MIN(amt, keylen);
 		for (i = 0; i < amt; i++) {
 			size_t dest = i * stride + (count - 1);
-			if (dest >= origkeylen)
+			if (dest >= origkeylen || i >= BCRYPT_HASHSIZE)
 				break;
 			key[dest] = out[i];
 		}
