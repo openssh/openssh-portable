6a32b2881f73e3fa825cd3df502b0edeeba703e7
rapier <rapier@psc.edu>
2015-11-10 14:09:30 -0500

    Fixed problem with None cipher not being advertised by server

--- sshd.c
+++ sshd.c
@@ -2570,19 +2570,20 @@ do_ssh2_kex(void)
 	struct kex *kex;
 	int r;
 
-	char *ciphers = options.ciphers;
-	if (ciphers == NULL && options.none_enabled == 1) {
-		debug("WARNING: None cipher enabled");
-		ciphers = KEX_ENCRYPT_INCLUDE_NONE;
-	}
-	myproposal[PROPOSAL_KEX_ALGS] = compat_kex_proposal(
-	    options.kex_algorithms);
-	myproposal[PROPOSAL_ENC_ALGS_CTOS] = compat_cipher_proposal(
-	    ciphers);
-	myproposal[PROPOSAL_ENC_ALGS_STOC] = compat_cipher_proposal(
-	    ciphers);
-	myproposal[PROPOSAL_MAC_ALGS_CTOS] =
-	    myproposal[PROPOSAL_MAC_ALGS_STOC] = options.macs;
+        myproposal[PROPOSAL_KEX_ALGS] = compat_kex_proposal(
+            options.kex_algorithms);
+        if (options.none_enabled == 1) {        
+                debug ("WARNING: None cipher enabled"); 
+                myproposal[PROPOSAL_ENC_ALGS_CTOS] =
+                        myproposal[PROPOSAL_ENC_ALGS_STOC] = KEX_ENCRYPT_INCLUDE_NONE;
+        } else {
+                myproposal[PROPOSAL_ENC_ALGS_CTOS] = compat_cipher_proposal(
+                    options.ciphers);
+                myproposal[PROPOSAL_ENC_ALGS_STOC] = compat_cipher_proposal(
+                    options.ciphers);
+        }
+        myproposal[PROPOSAL_MAC_ALGS_CTOS] =
+            myproposal[PROPOSAL_MAC_ALGS_STOC] = options.macs;
 
 	if (options.compression == COMP_NONE) {
 		myproposal[PROPOSAL_COMP_ALGS_CTOS] =
