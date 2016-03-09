e960051f9a264f682c4d2fefbeecffcfc66b0ddf
Darren Tucker <dtucker@zip.com.au>
2016-03-09 13:14:18 +1100

    Wrap stdint.h inside #ifdef HAVE_STDINT_H.

--- regress/misc/kexfuzz/kexfuzz.c
+++ regress/misc/kexfuzz/kexfuzz.c
@@ -10,7 +10,9 @@
 #include <sys/types.h>
 #include <sys/param.h>
 #include <stdio.h>
-#include <stdint.h>
+#ifdef HAVE_STDINT_H
+# include <stdint.h>
+#endif
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
