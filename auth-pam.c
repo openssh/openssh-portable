39c0cecaa188a37a2e134795caa68e03f3ced592
Darren Tucker <dtucker@zip.com.au>
2016-05-20 10:01:58 +1000

    Fix comment about sshpam_const and AIX.
    From mschwager via github.

--- auth-pam.c
+++ auth-pam.c
@@ -68,9 +68,9 @@
 
 /* OpenGroup RFC86.0 and XSSO specify no "const" on arguments */
 #ifdef PAM_SUN_CODEBASE
-# define sshpam_const		/* Solaris, HP-UX, AIX */
+# define sshpam_const		/* Solaris, HP-UX, SunOS */
 #else
-# define sshpam_const	const	/* LinuxPAM, OpenPAM */
+# define sshpam_const	const	/* LinuxPAM, OpenPAM, AIX */
 #endif
 
 /* Ambiguity in spec: is it an array of pointers or a pointer to an array? */
