9816fc5daee5ca924dd5c4781825afbaab728877
dtucker@openbsd.org <dtucker@openbsd.org>
2016-06-16 11:00:17 +0000

    upstream commit
    Include stdarg.h for va_copy as per man page.
    
    Upstream-ID: 105d6b2f1af2fbd9d91c893c436ab121434470bd

--- sshbuf-getput-basic.c
+++ sshbuf-getput-basic.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: sshbuf-getput-basic.c,v 1.5 2015/10/20 23:24:25 mmcc Exp $	*/
+/*	$OpenBSD: sshbuf-getput-basic.c,v 1.6 2016/06/16 11:00:17 dtucker Exp $	*/
 /*
  * Copyright (c) 2011 Damien Miller
  *
@@ -19,6 +19,8 @@
 #include "includes.h"
 
 #include <sys/types.h>
+
+#include <stdarg.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
