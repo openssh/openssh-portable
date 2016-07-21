fcd135c9df440bcd2d5870405ad3311743d78d97
dtucker@openbsd.org <dtucker@openbsd.org>
2016-07-21 01:39:35 +0000

    upstream commit
    Skip passwords longer than 1k in length so clients can't
    easily DoS sshd by sending very long passwords, causing it to spend CPU
    hashing them. feedback djm@, ok markus@.
    
    Brought to our attention by tomas.kuthan at oracle.com, shilei-c at
    360.cn and coredump at autistici.org
    
    Upstream-ID: d0af7d4a2190b63ba1d38eec502bc4be0be9e333

--- auth-passwd.c
+++ auth-passwd.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: auth-passwd.c,v 1.44 2014/07/15 15:54:14 millert Exp $ */
+/* $OpenBSD: auth-passwd.c,v 1.45 2016/07/21 01:39:35 dtucker Exp $ */
 /*
  * Author: Tatu Ylonen <ylo@cs.hut.fi>
  * Copyright (c) 1995 Tatu Ylonen <ylo@cs.hut.fi>, Espoo, Finland
@@ -66,6 +66,8 @@ extern login_cap_t *lc;
 #define DAY		(24L * 60 * 60) /* 1 day in seconds */
 #define TWO_WEEKS	(2L * 7 * DAY)	/* 2 weeks in seconds */
 
+#define MAX_PASSWORD_LEN	1024
+
 void
 disable_forwarding(void)
 {
@@ -86,6 +88,9 @@ auth_password(Authctxt *authctxt, const char *password)
 #if defined(USE_SHADOW) && defined(HAS_SHADOW_EXPIRE)
 	static int expire_checked = 0;
 #endif
+
+	if (strlen(password) > MAX_PASSWORD_LEN)
+		return 0;
 
 #ifndef HAVE_CYGWIN
 	if (pw->pw_uid == 0 && options.permit_root_login != PERMIT_YES)
