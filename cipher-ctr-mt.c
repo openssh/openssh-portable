93b2c7d9e029dc8bc418cd137060c1fb4019d517
Patrick McLean <patrickm@gaikai.com>
2016-09-07 11:42:29 -0700

    Add reader/writer lock on thread creation to parallel ctr cipher
    There is no gurantee that pthread_create will have filled in the thread
    id field when the thread starts, so there was a race condition.
    Sometimes the first thread would see c->tid[0] as 0, and not initialize
    the first queue, and the entire thing would deadlock. This uses a rwlock
    to make sure that the tid is filled in before trying to access it.

--- cipher-ctr-mt.c
+++ cipher-ctr-mt.c
@@ -141,6 +141,7 @@ struct ssh_aes_ctr_ctx
 	STATS_STRUCT(stats);
 	u_char		aes_counter[AES_BLOCK_SIZE];
 	pthread_t	tid[CIPHER_THREADS];
+	pthread_rwlock_t thread_lock;
 	int		state;
 	int		qidx;
 	int		ridx;
@@ -201,6 +202,7 @@ thread_loop(void *x)
 	struct kq *q;
 	int i;
 	int qidx;
+	pthread_t first_tid;
 
 	/* Threads stats on cancellation */
 	STATS_INIT(stats);
@@ -211,11 +213,15 @@ thread_loop(void *x)
 	/* Thread local copy of AES key */
 	memcpy(&key, &c->aes_ctx, sizeof(key));
 
+	pthread_rwlock_rdlock(&c->thread_lock);
+	first_tid = c->tid[0];
+	pthread_rwlock_unlock(&c->thread_lock);
+
 	/*
 	 * Handle the special case of startup, one thread must fill
 	 * the first KQ then mark it as draining. Lock held throughout.
 	 */
-	if (pthread_equal(pthread_self(), c->tid[0])) {
+	if (pthread_equal(pthread_self(), first_tid)) {
 		q = &c->q[0];
 		pthread_mutex_lock(&q->lock);
 		if (q->qstate == KQINIT) {
@@ -399,6 +405,7 @@ ssh_aes_ctr_init(EVP_CIPHER_CTX *ctx, const u_char *key, const u_char *iv,
 
 	if ((c = EVP_CIPHER_CTX_get_app_data(ctx)) == NULL) {
 		c = xmalloc(sizeof(*c));
+		pthread_rwlock_init(&c->thread_lock, NULL);
 
 		c->state = HAVE_NONE;
 		for (i = 0; i < NUMKQ; i++) {
@@ -446,7 +453,9 @@ ssh_aes_ctr_init(EVP_CIPHER_CTX *ctx, const u_char *key, const u_char *iv,
 		/* Start threads */
 		for (i = 0; i < CIPHER_THREADS; i++) {
 			debug("spawned a thread");
+			pthread_rwlock_wrlock(&c->thread_lock);
 			pthread_create(&c->tid[i], NULL, thread_loop, c);
+			pthread_rwlock_unlock(&c->thread_lock);
 		}
 		pthread_mutex_lock(&c->q[0].lock);
 		while (c->q[0].qstate != KQDRAINING)
@@ -483,7 +492,9 @@ ssh_aes_ctr_thread_reconstruction(EVP_CIPHER_CTX *ctx)
 	/* reconstruct threads */
 	for (i = 0; i < CIPHER_THREADS; i++) {
 		debug("spawned a thread");
+		pthread_rwlock_wrlock(&c->thread_lock);
 		pthread_create(&c->tid[i], NULL, thread_loop, c);
+		pthread_rwlock_unlock(&c->thread_lock);
 	}
 }
 
