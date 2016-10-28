bce58885160e5db2adda3054c3b81fe770f7285a
Darren Tucker <dtucker@zip.com.au>
2016-10-28 13:52:31 +1100

    Check if RIPEMD160 is disabled in OpenSSL.

--- digest-openssl.c
+++ digest-openssl.c
@@ -32,7 +32,8 @@
 #include "digest.h"
 #include "ssherr.h"
 
-#ifndef HAVE_EVP_RIPEMD160
+#if !defined(HAVE_EVP_RIPEMD160) || defined(OPENSSL_NO_RIPEMD) || \
+    defined(OPENSSL_NO_RMD160)
 # define EVP_ripemd160 NULL
 #endif /* HAVE_EVP_RIPEMD160 */
 #ifndef HAVE_EVP_SHA256
