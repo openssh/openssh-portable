7da751d8b007c7f3e814fd5737c2351440d78b4c
tb@openbsd.org <tb@openbsd.org>
2016-11-01 13:43:27 +0000

    upstream commit
    Clean up MALLOC_OPTIONS.  For the unittests, move
    MALLOC_OPTIONS and TEST_ENV to unittets/Makefile.inc.
    
    ok otto
    
    Upstream-Regress-ID: 890d497e0a38eeddfebb11cc429098d76cf29f12

--- regress/connect-privsep.sh
+++ regress/connect-privsep.sh
@@ -1,4 +1,4 @@
-#	$OpenBSD: connect-privsep.sh,v 1.7 2016/10/31 23:45:08 tb Exp $
+#	$OpenBSD: connect-privsep.sh,v 1.8 2016/11/01 13:43:27 tb Exp $
 #	Placed in the Public Domain.
 
 tid="proxy connect with privsep"
@@ -27,7 +27,7 @@ done
 # Because sandbox is sensitive to changes in libc, especially malloc, retest
 # with every malloc.conf option (and none).
 if [ -z "TEST_MALLOC_OPTIONS" ]; then
-	mopts="F G H J R S X < >"
+	mopts="C F G J R S U X < >"
 else
 	mopts=`echo $TEST_MALLOC_OPTIONS | sed 's/./& /g'`
 fi
