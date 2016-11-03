a927103a1c4d4cc3474d28e65193aa3325e97548
manojampalam <manojamp@microsoft.com>
2016-11-03 14:56:48 -0700

    Cleaned up unwanted code related to Kerb support in Windows

--- auth-krb5.c
+++ auth-krb5.c
@@ -30,15 +30,6 @@
 
 #include "includes.h"
 
-/*
- * We support only client side kerberos on Windows.
- */
-
-#ifdef WIN32_FIXME
-  #undef GSSAPI
-  #undef KRB5
-#endif
-
 #include <sys/types.h>
 #include <pwd.h>
 #include <stdarg.h>
