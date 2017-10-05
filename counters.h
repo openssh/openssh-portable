a0c37a7233bdb92b8160f8b2469eb9f87519d954
Chris Rapier <rapier@darksagan.psc.edu>
2017-10-05 15:14:26 -0400

    Forgot to add the counters.h file to the commit

---
+++ counters.h
@@ -0,0 +1,6 @@
+#ifndef COUNTERS_H
+#define COUNTERS_H
+u_long stdin_bytes;	/* Number of bytes written to stdin. */
+u_long fdout_bytes;	/* Number of stdout bytes read from program. */
+double start_time; /*start time for server connection */
+#endif
