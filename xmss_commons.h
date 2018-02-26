afd830847a82ebbd5aeab05bad6d2c8ce74df1cd
dtucker@openbsd.org <dtucker@openbsd.org>
2018-02-26 03:03:05 +0000

    upstream: Add newline at end of file to prevent compiler warnings.
    OpenBSD-Commit-ID: 52f247d4eafe840c7c14c8befa71a760a8eeb063

--- xmss_commons.h
+++ xmss_commons.h
@@ -12,7 +12,7 @@ Public domain.
 #ifdef HAVE_STDINT_H
 #include <stdint.h>
 #endif
-
+#endif
 void to_byte(unsigned char *output, unsigned long long in, uint32_t bytes);
 #if 0
 void hexdump(const unsigned char *a, size_t len);
