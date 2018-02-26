3b36bed3d26f17f6a2b7e036e01777770fe1bcd4
dtucker@openbsd.org <dtucker@openbsd.org>
2018-02-26 12:14:53 +0000

    upstream: Remove unneeded (local) include. ok markus@
    OpenBSD-Commit-ID: 132812dd2296b1caa8cb07d2408afc28e4e60f93

--- xmss_wots.h
+++ xmss_wots.h
@@ -1,5 +1,5 @@
 #ifdef WITH_XMSS
-/* $OpenBSD: xmss_wots.h,v 1.2 2018/02/26 03:56:44 dtucker Exp $ */
+/* $OpenBSD: xmss_wots.h,v 1.3 2018/02/26 12:14:53 dtucker Exp $ */
 /*
 wots.h version 20160722
 Andreas Hülsing
