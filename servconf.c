d6e5def308610f194c0ec3ef97a34a3e9630e190
djm@openbsd.org <djm@openbsd.org>
2019-03-25 22:33:44 +0000

    upstream: whitespace
    OpenBSD-Commit-ID: 106e853ae8a477e8385bc53824d3884a8159db07

--- servconf.c
+++ servconf.c
@@ -1,5 +1,5 @@
 
-/* $OpenBSD: servconf.c,v 1.349 2019/03/06 22:14:23 dtucker Exp $ */
+/* $OpenBSD: servconf.c,v 1.350 2019/03/25 22:33:44 djm Exp $ */
 /*
  * Copyright (c) 1995 Tatu Ylonen <ylo@cs.hut.fi>, Espoo, Finland
  *                    All rights reserved
@@ -470,7 +470,6 @@ fill_default_server_options(ServerOptions *options)
 		options->compression = 0;
 	}
 #endif
-
 }
 
 /* Keyword tokens. */
