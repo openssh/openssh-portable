a16253be832dace196a09843b8d97f974b6ea715
Chris Rapier <rapier@darksagan.psc.edu>
2019-11-08 09:37:36 -0500

    crank version

--- version.h
+++ version.h
@@ -3,5 +3,5 @@
 #define SSH_VERSION	"OpenSSH_8.0"
 
 #define SSH_PORTABLE	"p1"
-#define SSH_HPN         "-hpn14v16"
+#define SSH_HPN         "-hpn14v18"
 #define SSH_RELEASE	SSH_VERSION SSH_PORTABLE SSH_HPN
