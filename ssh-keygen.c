26cb128b31efdd5395153f4943f5be3eddc07033
djm@openbsd.org <djm@openbsd.org>
2019-11-25 00:57:27 +0000

    upstream: Print a key touch reminder when generating a security
    key. Most keys require a touch to authorize the operation.
    
    OpenBSD-Commit-ID: 7fe8b23edbf33e1bb81741b9f25e9a63be5f6b68

--- ssh-keygen.c
+++ ssh-keygen.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: ssh-keygen.c,v 1.372 2019/11/25 00:55:58 djm Exp $ */
+/* $OpenBSD: ssh-keygen.c,v 1.373 2019/11/25 00:57:27 djm Exp $ */
 /*
  * Author: Tatu Ylonen <ylo@cs.hut.fi>
  * Copyright (c) 1994 Tatu Ylonen <ylo@cs.hut.fi>, Espoo, Finland
@@ -3294,6 +3294,11 @@ main(int argc, char **argv)
 #ifndef ENABLE_SK
 		fatal("Security key support was disabled at compile time");
 #else /* ENABLE_SK */
+		if (!quiet) {
+			printf("You may need to touch your security key "
+			    "to authorize key generation.\n");
+		}
+		fflush(stdout);
 		if (sshsk_enroll(type, sk_provider,
  		    cert_key_id == NULL ? "ssh:" : cert_key_id,
  		    sk_flags, NULL, &private, NULL) != 0)
