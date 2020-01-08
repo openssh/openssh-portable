a991cc5ed5a7c455fefe909a30cf082011ef5dff
Khem Raj <raj.khem@gmail.com>
2020-01-07 16:26:45 -0800

    seccomp: Allow clock_gettime64() in sandbox.
    This helps sshd accept connections on mips platforms with
    upcoming glibc ( 2.31 )

--- sandbox-seccomp-filter.c
+++ sandbox-seccomp-filter.c
@@ -248,6 +248,9 @@ static const struct sock_filter preauth_insns[] = {
 #ifdef __NR_clock_nanosleep_time64
 	SC_ALLOW(__NR_clock_nanosleep_time64),
 #endif
+#ifdef __NR_clock_gettime64
+	SC_ALLOW(__NR_clock_gettime64),
+#endif
 #ifdef __NR__newselect
 	SC_ALLOW(__NR__newselect),
 #endif
