31c860a0212af2d5b6a129e3e8fcead51392ee1d
markus@openbsd.org <markus@openbsd.org>
2020-03-06 18:14:13 +0000

    upstream: pkcs11_register_provider: return < 0 on error; ok djm
    OpenBSD-Commit-ID: cfc8321315b787e4d40da4bdb2cbabd4154b0d97

--- ssh-pkcs11.c
+++ ssh-pkcs11.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: ssh-pkcs11.c,v 1.47 2020/01/25 00:03:36 djm Exp $ */
+/* $OpenBSD: ssh-pkcs11.c,v 1.48 2020/03/06 18:14:13 markus Exp $ */
 /*
  * Copyright (c) 2010 Markus Friedl.  All rights reserved.
  * Copyright (c) 2014 Pedro Martelletto. All rights reserved.
@@ -1627,6 +1627,8 @@ fail:
 	}
 	if (handle)
 		dlclose(handle);
+	if (ret > 0)
+		ret = -1;
 	return (ret);
 }
 
