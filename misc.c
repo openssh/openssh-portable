6c6072ba8b079e6f5caa38b011a6f4570c14ed38
Darren Tucker <dtucker@dtucker.net>
2020-05-01 15:09:26 +1000

    See if SA_RESTART signals will interrupt select().
    On some platforms (at least older HP-UXes such as 11.11, possibly others)
    setting SA_RESTART on signal handers will cause it to not interrupt
    select(), at least for calls that do not specify a timeout.  Try to
    detect this and if found, don't use SA_RESTART.
    
    POSIX says "If SA_RESTART has been set for the interrupting signal, it
    is implementation-dependent whether select() restarts or returns with
    [EINTR]" so this behaviour is within spec.

--- misc.c
+++ misc.c
@@ -2258,8 +2258,10 @@ ssh_signal(int signum, sshsig_t handler)
 	memset(&sa, 0, sizeof(sa));
 	sa.sa_handler = handler;
 	sigfillset(&sa.sa_mask);
+#if defined(SA_RESTART) && !defined(NO_SA_RESTART)
 	if (signum != SIGALRM)
 		sa.sa_flags = SA_RESTART;
+#endif
 	if (sigaction(signum, &sa, &osa) == -1) {
 		debug3("sigaction(%s): %s", strsignal(signum), strerror(errno));
 		return SIG_ERR;
