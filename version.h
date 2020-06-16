76d47c21f00240566cd29b80335548bcc1033479
Chris Rapier <rapier@darksagan.myfiosgateway.com>
2020-06-16 12:58:16 -0400

    crank version number

--- version.h
+++ version.h
@@ -3,5 +3,5 @@
 #define SSH_VERSION	"OpenSSH_8.3"
 
 #define SSH_PORTABLE	"p1"
-#define SSH_HPN         "-hpn14v20"
+#define SSH_HPN         "-hpn14v22"
 #define SSH_RELEASE	SSH_VERSION SSH_PORTABLE SSH_HPN
