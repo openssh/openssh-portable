eb122b1eebe58b29a83a507ee814cbcf8aeded1b
Darren Tucker <dtucker@dtucker.net>
2020-08-07 15:11:42 +1000

    Add ability to specify exact test target.

---
+++ .github/run_test.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+TARGETS=$@
+
+TEST_TARGET="tests"
+LTESTS=""  # all tests by default
+
+set -ex
+
+for TARGET in $TARGETS; do
+    case $TARGET in
+    --without-openssl)
+        # When built without OpenSSL we can't do the file-based RSA key tests.
+        TEST_TARGET=t-exec
+        ;;
+    esac
+done
+
+if [ -z "$LTESTS" ]; then
+    make $TEST_TARGET
+else
+    make $TEST_TARGET LTESTS="$LTESTS"
+fi
