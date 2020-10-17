999d7cb79a3a73d92a6dfbf174c33da0d984c7a2
Damien Miller <djm@mindrot.org>
2020-10-17 22:47:52 +1100

    sync regress/misc/sk-dummy/fatal.c

--- regress/misc/sk-dummy/fatal.c
+++ regress/misc/sk-dummy/fatal.c
@@ -7,14 +7,17 @@
 #include <stdarg.h>
 #include <unistd.h>
 
-void
-sshfatal(const char *file, const char *func, int line, const char *fmt, ...);
+void sshfatal(const char *file, const char *func, int line, int showfunc,
+    LogLevel level, const char *fmt, ...);
 
 void
-sshfatal(const char *file, const char *func, int line, const char *fmt, ...)
+sshfatal(const char *file, const char *func, int line, int showfunc,
+    LogLevel level, const char *fmt, ...)
 {
 	va_list ap;
 
+	if (showfunc)
+		fprintf(stderr, "%s: ", func);
 	va_start(ap, fmt);
 	vfprintf(stderr, fmt, ap);
 	va_end(ap);
