aa99b2d9a3e45b943196914e8d8bf086646fdb54
Darren Tucker <dtucker@dtucker.net>
2021-06-04 23:41:29 +1000

    Clear notify_pipe from readset if present.
    Prevents leaking an implementation detail to the caller.

--- openbsd-compat/bsd-pselect.c
+++ openbsd-compat/bsd-pselect.c
@@ -114,9 +114,11 @@ notify_done(fd_set *readset)
 {
 	char c;
 
-	if (notify_pipe[0] != -1 && FD_ISSET(notify_pipe[0], readset))
+	if (notify_pipe[0] != -1 && FD_ISSET(notify_pipe[0], readset)) {
 		while (read(notify_pipe[0], &c, 1) != -1)
 			debug2_f("reading");
+		FD_CLR(notify_pipe[0], readset);
+	}
 }
 
 /*ARGSUSED*/
