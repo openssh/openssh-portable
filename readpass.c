280589c4b11c434f940a655dec1129a8c75aa2a2
Tobias Stoeckmann <tobias@stoeckmann.org>
2021-10-22 19:19:08 +0200

    Avoid kill with -1 argument.
    The out_ctx label can be reached before fork has been called. If this
    happens, then kill -1 would be called, sending SIGTERM to all processes
    reachable by the current process.
    
    This is definitely not intended behavior.

--- readpass.c
+++ readpass.c
@@ -286,7 +286,8 @@ notify_start(int force_askpass, const char *fmt, ...)
 	}
  out_ctx:
 	if ((ret = calloc(1, sizeof(*ret))) == NULL) {
-		kill(pid, SIGTERM);
+		if (pid != -1)
+			kill(pid, SIGTERM);
 		fatal_f("calloc failed");
 	}
 	ret->pid = pid;
