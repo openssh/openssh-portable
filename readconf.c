48dc4cb9c3c31f18ecd264a5d270f0d026bc02ac
C. Jacobs <515451+ProBackup-nl@users.noreply.github.com>
2022-01-06 11:40:13 +0100

    load dsa key last
    The order in which keys are loaded into ssh-agent defines the order in which keys are used in ssh. Thus it might be wiser to load the relative less secure dsa last.

--- readconf.c
+++ readconf.c
@@ -2533,7 +2533,6 @@ fill_default_options(Options * options)
 	}
 	if (options->num_identity_files == 0) {
 		add_identity_file(options, "~/", _PATH_SSH_CLIENT_ID_RSA, 0);
-		add_identity_file(options, "~/", _PATH_SSH_CLIENT_ID_DSA, 0);
 #ifdef OPENSSL_HAS_ECC
 		add_identity_file(options, "~/", _PATH_SSH_CLIENT_ID_ECDSA, 0);
 		add_identity_file(options, "~/",
@@ -2544,6 +2543,7 @@ fill_default_options(Options * options)
 		add_identity_file(options, "~/",
 		    _PATH_SSH_CLIENT_ID_ED25519_SK, 0);
 		add_identity_file(options, "~/", _PATH_SSH_CLIENT_ID_XMSS, 0);
+		add_identity_file(options, "~/", _PATH_SSH_CLIENT_ID_DSA, 0);
 	}
 	if (options->escape_char == -1)
 		options->escape_char = '~';
