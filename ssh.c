c20d41a2d218f2b77f3f6a1393660eeb918e0a42
Chris Rapier <rapier@kilo.psc.edu>
2022-05-06 09:14:46 -0400

    Special version number and include HPNBufferLimit in sshd_config

--- ssh.c
+++ ssh.c
@@ -2162,15 +2162,15 @@ ssh_session2_open(struct ssh *ssh)
 	    window, packetmax, CHAN_EXTENDED_WRITE,
 	    "client-session", CHANNEL_NONBLOCK_STDIO);
 
+	/* TODO: Is this the right place for these options? */
 	if (options.tcp_rcv_buf_poll > 0 && !options.hpn_disabled) {
 		c->dynamic_window = 1;
 		debug("Enabled Dynamic Window Scaling");
 	}
 
-	if (options.hpn_buffer_limit) {
+	if (options.hpn_buffer_limit)
 		c->hpn_buffer_limit = 1;
-		debug("LIMITS!");
-	}
+
 		
 	debug3_f("channel_new: %d", c->self);
 
