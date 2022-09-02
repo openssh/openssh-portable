d6cb61facb9ca7d4c3aac32c4121913ccfbb30db
ferrumgate <admin@ferrumgate.com>
2022-09-02 23:03:35 +0300

    server dockerized

---
+++ ferrum/docker/docker.build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# docker build script
+
+
+set -e
+
+
+
+while getopts i: flag
+do
+    case "${flag}" in
+        i) IMAGE_NAME=${OPTARG};;
+    esac
+done
+
+#read -p 'enter version:' version
+version=$(cat ./ferrum/ferrum.h |grep FERRUM_SECURE_SERVER_VERSION|cut -d' ' -f3|tr -d '"')
+
+# if not set
+if [ -z $IMAGE_NAME ]
+then 
+IMAGE_NAME=secure.server
+fi
+
+echo $IMAGE_NAME is building
+docker build -f ./ferrum/docker/dockerfile -t $IMAGE_NAME .
