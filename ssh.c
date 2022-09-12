c787b9a9d9dc554bcab8aef16324e862d9eaa5a2
Chris Rapier <rapier@psc.edu>
2022-09-12 17:12:23 -0400

    Removed errant/unneeded EVP_CIPHER dec in ssh.c

--- ssh.c
+++ ssh.c
@@ -1769,8 +1769,6 @@ control_persist_detach(void)
 	setproctitle("%s [mux]", options.control_path);
 }
 
-extern const EVP_CIPHER *evp_aes_ctr_mt(void);
-
 /* Do fork() after authentication. Used by "ssh -f" */
 static void
 fork_postauth(void)
