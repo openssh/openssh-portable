openssh/openssh-portable

commit c64b62338b46ffa08839f05f21ad69fa6234dc17

skip bsd-poll.h if poll.h found; ok dtucker

openbsd-compat/bsd-poll.h
@@ -35,7 +35,7 @@
 # include <poll.h>
 #elif HAVE_SYS_POLL_H
 # include <sys/poll.h>
-#endif
+#else
 
 #ifndef HAVE_STRUCT_POLLFD_FD
 typedef struct pollfd {
@@ -73,5 +73,5 @@ int   poll(struct pollfd *, nfds_t, int);
 #ifndef HAVE_PPOLL
 int   ppoll(struct pollfd *, nfds_t, const struct timespec *, const sigset_t *);
 #endif
-
+#endif /* !HAVE_POLL_H && !HAVE_SYS_POLL_H */
 #endif /* !_COMPAT_POLL_H_ */

commit 195e5a65fd793a738ea8451ebfdd1919db5aff3e

revert c64b62338b4 and guard POLL* defines instead
c64b62338b4 broke OSX builds, which do have poll.h but lack ppoll(2)
Spotted by dtucker

openbsd-compat/bsd-poll.h
@@ -35,7 +35,7 @@
 # include <poll.h>
 #elif HAVE_SYS_POLL_H
 # include <sys/poll.h>
-#else
+#endif
 
 #ifndef HAVE_STRUCT_POLLFD_FD
 typedef struct pollfd {
@@ -44,12 +44,25 @@ typedef struct pollfd {
 	short	revents;
 } pollfd_t;
 
-#define	POLLIN		0x0001
-#define	POLLPRI		0x0002
-#define	POLLOUT		0x0004
-#define	POLLERR		0x0008
-#define	POLLHUP		0x0010
-#define	POLLNVAL	0x0020
+#ifndef POLLIN
+# define POLLIN		0x0001
+#endif
+#ifndef POLLPRI
+# define POLLPRI	0x0002
+#endif
+#ifndef POLLOUT
+# define POLLOUT	0x0004
+#endif
+#ifndef POLLERR
+# define POLLERR	0x0008
+#endif
+#ifndef POLLHUP
+# define POLLHUP	0x0010
+#endif
+#ifndef POLLNVAL
+# define POLLNVAL	0x0020
+#endif
+
 #if 0
 /* the following are currently not implemented */
 #define	POLLRDNORM	0x0040
@@ -73,5 +86,5 @@ int   poll(struct pollfd *, nfds_t, int);
 #ifndef HAVE_PPOLL
 int   ppoll(struct pollfd *, nfds_t, const struct timespec *, const sigset_t *);
 #endif
-#endif /* !HAVE_POLL_H && !HAVE_SYS_POLL_H */
+
 #endif /* !_COMPAT_POLL_H_ */