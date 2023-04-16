59c3b615d4a2699790e033f979ea88a86fd179ec
Connor Stewart <connorstewarty@gmail.com>
2023-04-16 13:25:53 -0500

    50D-Instantiate variables to fix free()

--- ssh_api.c
+++ ssh_api.c
@@ -335,7 +335,8 @@ _ssh_read_banner(struct ssh *ssh, struct sshbuf *banner)
 	const char *mismatch = "Protocol mismatch.\r\n";
 	const u_char *s = sshbuf_ptr(input);
 	u_char c;
-	char *cp = NULL, *remote_version = NULL;
+	char *cp = (u_char*) malloc(20 * sizeof(u_char));
+	char *remote_version = (u_char*) malloc(20 * sizeof(u_char));
 	int r = 0, remote_major, remote_minor, expect_nl;
 	size_t n, j;
 
@@ -414,7 +415,7 @@ _ssh_read_banner(struct ssh *ssh, struct sshbuf *banner)
 int
 _ssh_send_banner(struct ssh *ssh, struct sshbuf *banner)
 {
-	char *cp;
+	char *cp = (char*) malloc(20 * sizeof(char));
 	int r;
 
 	if ((r = sshbuf_putf(banner, "SSH-2.0-%.100s\r\n", SSH_VERSION)) != 0)
@@ -519,7 +520,9 @@ int
 _ssh_order_hostkeyalgs(struct ssh *ssh)
 {
 	struct key_entry *k;
-	char *orig, *avail, *oavail = NULL, *alg, *replace = NULL;
+	char *orig = (char*) malloc(20 * sizeof(char));
+	char *oavail = (char*) malloc(20 * sizeof(char));
+	char *avail = NULL, *alg, *replace = NULL;
 	char **proposal;
 	size_t maxlen;
 	int ktype, r;
