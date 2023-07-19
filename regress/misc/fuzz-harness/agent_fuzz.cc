f3f56df8ec476b2de6cbdbdfdb77a2a61087829d
Damien Miller <djm@mindrot.org>
2023-07-19 12:07:18 +1000

    agent_fuzz doesn't want stdint.h conditionalised

--- regress/misc/fuzz-harness/agent_fuzz.cc
+++ regress/misc/fuzz-harness/agent_fuzz.cc
@@ -1,9 +1,7 @@
 // cc_fuzz_target test for ssh-agent.
 extern "C" {
 
-#ifdef HAVE_STDINT_H
-# include <stdint.h>
-#endif
+#include <stdint.h>
 #include <sys/types.h>
 
 extern void test_one(const uint8_t* s, size_t slen);
