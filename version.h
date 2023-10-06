d625c81f89f822d6672030c3d8c2ca4b66bc0d60
Chris Rapier <rapier@psc.edu>
2023-10-06 17:11:06 -0400

    Crank version number

--- version.h
+++ version.h
@@ -3,5 +3,5 @@
 #define SSH_VERSION	"OpenSSH_9.5"
 
 #define SSH_PORTABLE	"p1"
-#define SSH_HPN         "-hpn18.1.0"
+#define SSH_HPN         "-hpn18.2.0"
 #define SSH_RELEASE	SSH_VERSION SSH_PORTABLE SSH_HPN
