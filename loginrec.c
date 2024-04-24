fee745e444116e026b6116ab89404d577b507f56
Pavel Miadzvedzeu <pmiadzvedzeu@gmail.com>
2024-04-24 10:19:56 +0300

    [loginrec.c] Fix "undeclared 'ut'" error by replacing it with 'utx'

--- loginrec.c
+++ loginrec.c
@@ -1033,7 +1033,7 @@ utmpx_perform_login(struct logininfo *li)
 		return (0);
 	}
 # else
-	if (!utmpx_write_direct(li, &ut)) {
+	if (!utmpx_write_direct(li, &utx)) {
 		logit("%s: utmp_write_direct() failed", __func__);
 		return (0);
 	}
