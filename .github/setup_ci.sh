6165293d8ed76660d8321094a7311c6261628a4b
Shubham Mittal <smittals@amazon.com>
2025-02-05 18:24:44 -0800

    sudo priveleges

--- .github/setup_ci.sh
+++ .github/setup_ci.sh
@@ -247,7 +247,7 @@ fi
 if [ ! -z "${INSTALL_AWSLC}" ]; then
     (cd ${HOME} && git clone --depth 1 --branch v1.42.0 https://github.com/aws/aws-lc.git &&
      cd ${HOME}/aws-lc && mkdir build && cd build &&
-     cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=OFF .. && ninja install &&
+     cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=OFF .. && sudo ninja install &&
      mkdir -p /opt/aws-lc/lib &&
      cp ${HOME}/aws-lc/build/crypto/libcrypto.a /opt/aws-lc/lib &&
      cp -r ${HOME}/aws-lc/include /opt/aws-lc && 
