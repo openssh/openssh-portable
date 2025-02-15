f519e71fb7a46314ae16e2a75490649dc0bd01a2
Damien Miller <djm@mindrot.org>
2025-02-15 13:12:40 +1100

    depend

--- version.h
+++ version.h
@@ -1,7 +1,6 @@
 /* $OpenBSD: version.h,v 1.104 2025/02/15 01:52:07 djm Exp $ */
 
 #define SSH_VERSION	"OpenSSH_9.9"
-#define SSH_RELEASE    SSH_VERSION
 
 #define SSH_PORTABLE	"p1"
 #define SSH_RELEASE	SSH_VERSION SSH_PORTABLE
