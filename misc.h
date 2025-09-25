4fddebe7f524b3403c876c3b399d5ce7ce3390a6
djm@openbsd.org <djm@openbsd.org>
2025-09-25 06:33:19 +0000

    upstream: add some functions to free various structs, including
    channels data and packet state; ok dtucker@ tb@
    
    OpenBSD-Commit-ID: a8b3705309d632cdae370d4147a03e703087b0d1

--- misc.h
+++ misc.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: misc.h,v 1.111 2025/05/05 02:48:06 djm Exp $ */
+/* $OpenBSD: misc.h,v 1.112 2025/09/25 06:33:19 djm Exp $ */
 
 /*
  * Author: Tatu Ylonen <ylo@cs.hut.fi>
@@ -113,6 +113,7 @@ char	*get_homedir(void);
 void	 sock_set_v6only(int);
 
 struct passwd *pwcopy(struct passwd *);
+void	 pwfree(struct passwd *); /* NB. only use with pwcopy */
 const char *ssh_gai_strerror(int);
 
 typedef void privdrop_fn(struct passwd *);
