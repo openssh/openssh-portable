bb2703365ede3b4e13fdfa1c250ac88408e75f38
dtucker@openbsd.org <dtucker@openbsd.org>
2026-02-09 21:21:39 +0000

    upstream: Remove now-unused openssl includes since sshd.c no longer
    needs them, even when built with OpenSSL.
    
    OpenBSD-Commit-ID: ceaa0394db1520e92d75c37eea58130d44ba93c9

--- sshd.c
+++ sshd.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: sshd.c,v 1.625 2026/02/08 15:28:01 dtucker Exp $ */
+/* $OpenBSD: sshd.c,v 1.626 2026/02/09 21:21:39 dtucker Exp $ */
 /*
  * Copyright (c) 2000, 2001, 2002 Markus Friedl.  All rights reserved.
  * Copyright (c) 2002 Niels Provos.  All rights reserved.
@@ -50,12 +50,6 @@
 #include <string.h>
 #include <unistd.h>
 #include <limits.h>
-
-#ifdef WITH_OPENSSL
-#include <openssl/evp.h>
-#include <openssl/rand.h>
-#include "openbsd-compat/openssl-compat.h"
-#endif
 
 #ifdef HAVE_SECUREWARE
 #include <sys/security.h>
