05ec77c14b9143b4d49f05a8dcd1366a92b9024e
djm@openbsd.org <djm@openbsd.org>
2026-08-08 07:27:54 +0000

    upstream: remove mlkem768brainpoolp256r1-sha256; requested by
    markus
    
    OpenBSD-Commit-ID: 0fcb5943abe5476e42983dcfc4c400e39e5db6a7

--- kex-names.c
+++ kex-names.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: kex-names.c,v 1.9 2026/07/27 12:31:09 markus Exp $ */
+/* $OpenBSD: kex-names.c,v 1.10 2026/08/08 07:27:54 djm Exp $ */
 /*
  * Copyright (c) 2000, 2001 Markus Friedl.  All rights reserved.
  *
@@ -69,10 +69,6 @@ static const struct kexalg kexalgs[] = {
 	    SSH_DIGEST_SHA512, KEX_NOT_PQ },
 	{ KEX_MLKEM768NISTP256_SHA256, KEX_KEM_MLKEM768ECDH_SHA256,
 	    NID_X9_62_prime256v1, SSH_DIGEST_SHA256, KEX_IS_PQ },
-# ifdef USE_BRAINPOOLP256R1
-	{ KEX_MLKEM768BRAINPOOLP256R1_SHA256, KEX_KEM_MLKEM768ECDH_SHA256,
-	    NID_brainpoolP256r1, SSH_DIGEST_SHA256, KEX_IS_PQ },
-# endif /* USE_BRAINPOOLP256R1 */
 #endif /* WITH_OPENSSL */
 #if defined(HAVE_EVP_SHA256) || !defined(WITH_OPENSSL)
 	{ KEX_CURVE25519_SHA256, KEX_C25519_SHA256, 0, SSH_DIGEST_SHA256, KEX_NOT_PQ },
